694e7ec42663000f5d67808a174618f182319236
Chuo-Yun Yang <cyang432@gatech.edu>
2019-03-08 01:46:58 -0500

    marta api

--- backend/main.go
+++ backend/main.go
@@ -22,8 +22,26 @@ func handleReq() {
 	r := mux.NewRouter()
 	r.HandleFunc("/bike", getBikeStations(sendBikeStations)).Methods("GET")
 	r.HandleFunc("/dist/origin/bike", getBikeStations(computeOriginBike)).Methods("GET")
+	r.HandleFunc("/dist/origin/marta", getMartaStations(computeOriginMarta)).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getMartaStations(h http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		content, err := getDataFromApi("https://opendata.arcgis.com/datasets/7b752dcfca54486c8290b399340a407c_17.geojson")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		stationCord := buildMartaMap(content)
+		context.Set(r, "stationCord", stationCord)
+		// fmt.Println(stationCord)
+
+		h.ServeHTTP(w, r)
+	})
+
 }
 
 func getBikeStations(h http.HandlerFunc) http.HandlerFunc {
@@ -54,6 +72,29 @@ func getDataFromApi(url string) ([]byte, error) {
 		return nil, errors.New("Cannot get body!")
 	}
 	return body, nil
+}
+
+func buildMartaMap(content []byte) map[string][]float64 {
+	stationCord := map[string][]float64{}
+
+	resJson := map[string]interface{}{}
+	json.Unmarshal(content, &resJson)
+	for _, station := range resJson["features"].([]interface{}) {
+		name := ""
+		cord := []float64{}
+		for keyProp, prop := range station.(map[string]interface{}) {
+			if keyProp == "properties" {
+				name = prop.(map[string]interface{})["STATION"].(string)
+			} else if keyProp == "geometry" {
+				cord = append(cord, prop.(map[string]interface{})["coordinates"].([]interface{})[1].(float64))
+				cord = append(cord, prop.(map[string]interface{})["coordinates"].([]interface{})[0].(float64))
+			}
+		}
+		// fmt.Println(station_Name)
+		// fmt.Println(station_Cord)
+		stationCord[name] = cord
+	}
+	return stationCord
 }
 
 func buildBikeMap(content []byte) map[string][]float64 {
@@ -90,6 +131,70 @@ func sendBikeStations(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "%v", stationCord)
 }
 
+func computeOriginMarta(w http.ResponseWriter, r *http.Request) {
+	allData := context.GetAll(r)
+	stationCord := allData["stationCord"]
+	// fmt.Println(r.URL.Query().Get("k"))
+	k := r.URL.Query().Get("k")
+	lat := r.URL.Query().Get("lat")
+	lng := r.URL.Query().Get("lng")
+	flat, err := strconv.ParseFloat(lat, 64)
+	if err != nil {
+		http.Error(w, "lat parse error", http.StatusBadRequest)
+		return
+	}
+	flng, err := strconv.ParseFloat(lng, 64)
+	if err != nil {
+		http.Error(w, "lng parse error", http.StatusBadRequest)
+		return
+	}
+	ik, err := strconv.Atoi(k)
+	if err != nil {
+		http.Error(w, "k parse error", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(ik, flat, flng)
+	fmt.Println(stationCord)
+
+	type martaCord struct {
+		Key   string
+		Value []float64
+	}
+
+	martaVals := []martaCord{}
+	origin := []float64{flat, flng}
+	for k, v := range stationCord.(map[string][]float64) {
+		v = append(v, calcDist(origin, v))
+		martaVals = append(martaVals, martaCord{k, v})
+	}
+
+	sort.Slice(martaVals, func(i, j int) bool {
+		// d1 := calcDist(origin, bikeVals[i].Value)
+		// d2 := calcDist(origin, bikeVals[j].Value)
+		// fmt.Println(d1, d2)
+		return martaVals[i].Value[2] < martaVals[j].Value[2]
+		// return ss[i].Value > ss[j].Value
+	})
+
+	type resObj struct {
+		Name  string
+		Dist  float64
+		Coord []float64
+	}
+	resCord := []resObj{}
+	for _, martaCord := range martaVals {
+		if ik == 0 {
+			break
+		}
+		// fmt.Printf("%s, %v\n", bikeCord.Key, bikeCord.Value)
+		resCord = append(resCord, resObj{martaCord.Key, martaCord.Value[2], []float64{martaCord.Value[0], martaCord.Value[1]}})
+		ik--
+	}
+	fmt.Println(resCord)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resCord)
+}
 func computeOriginBike(w http.ResponseWriter, r *http.Request) {
 	allData := context.GetAll(r)
 	stationCord := allData["stationCord"]
