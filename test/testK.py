084e8fa66c5edf06502d53588ecb699f294b1050
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2019-04-07 08:38:56 -0400

    add time stamp

--- test/testK.py
+++ test/testK.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import os
+import time
 
 def getBikeMarta(k):
     fileName = "output" + str(k) + ".txt"
@@ -9,6 +10,8 @@ def getBikeMarta(k):
         print("Removed {}".format(fileName))
 
     with open('inputList.txt', mode='r') as input:
+        start = time.clock()
+
         while True:
             coord1 = input.readline()
             if not coord1:
@@ -21,6 +24,10 @@ def getBikeMarta(k):
             r = requests.get("http://3.18.223.207:8080/dist/walk/bike/walk?k=" + str(k) + "&lat1=" + coord1[0] + "&lng1=" + coord1[1] + "&lat2=" + coord2[0] + "&lng2=" + coord2[1])
             with open('output' + k + '.txt', mode='a') as output:
                 output.write("{}\n".format(r.json()[0]))
+
+        end = time.clock()
+        with open('output' + k + '.txt', mode='a') as output:
+                output.write("Spent %.2f seconds \n"%(end - start))
 
 if __name__ == "__main__":
     getBikeMarta(sys.argv[1])
